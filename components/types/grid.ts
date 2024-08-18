1869e2ca4983cd3009c90b798dbf15201e0b7cb4
ngounphanny <**Hello123>
2024-08-18 16:35:49 +0700

    .

--- components/types/grid.ts
+++ components/types/grid.ts
@@ -4,5 +4,5 @@ export interface GridProps {
   defaultColumnWidth?: string | number;
   containerClass?: string;
   gridClass?: string;
-  rowClass?: ((row: SupaRow) => string | undefined) | undefined;
+  // rowClass?: ((row: SupaRow) => string | undefined) | undefined;
 }
